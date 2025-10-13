52648a0285ad028c7ce2d39d70c86c814ea2d696
Faur Ioan-Aurel <fioan89@gmail.com>
2025-10-13 23:43:51 +0300

    impl: models for dynamic client registration
    OAuth allows programatic client registration for apps like Coder Toolbox
    via the DCR endpoint which requires a name for the client app, the requested
    scopes, redirect URI, etc... DCR replies back with a similar structure but
    in addition it returs two very important properties: client_id - a unique
    client identifier string and also a client_secret - a secret string value
    used by clients to authenticate to the token endpoint.

---
+++ src/main/kotlin/com/coder/toolbox/oauth/ClientRegistrationResponse.kt
@@ -0,0 +1,21 @@
+package com.coder.toolbox.oauth
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * DCR response
+ */
+@JsonClass(generateAdapter = true)
+data class ClientRegistrationResponse(
+    @field:Json(name = "client_id") val clientId: String,
+    @field:Json(name = "client_secret") val clientSecret: String,
+    @field:Json(name = "client_name") val clientName: String,
+    @field:Json(name = "redirect_uris") val redirectUris: List<String>,
+    @field:Json(name = "grant_types") val grantTypes: List<String>,
+    @field:Json(name = "response_types") val responseTypes: List<String>,
+    @field:Json(name = "scope") val scope: String,
+    @field:Json(name = "token_endpoint_auth_method") val tokenEndpointAuthMethod: String,
+    @field:Json(name = "client_id_issued_at") val clientIdIssuedAt: Long?,
+    @field:Json(name = "client_secret_expires_at") val clientSecretExpiresAt: Long?
+)
