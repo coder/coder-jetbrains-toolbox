5048e1b8705e54a3cca357aa075f4e372bccd3a5
Faur Ioan-Aurel <fioan89@gmail.com>
2026-03-04 00:19:20 +0200

    impl: use a buffer to read input streams
    Using a buffer instead of reading byte-by-byte is a more efficient mechanism.

--- src/main/kotlin/com/coder/toolbox/util/Hash.kt
+++ src/main/kotlin/com/coder/toolbox/util/Hash.kt
@@ -5,17 +5,19 @@ import java.io.InputStream
 import java.security.DigestInputStream
 import java.security.MessageDigest
 
+private const val BUFFER_SIZE = 8192
+
 fun ByteArray.toHex() = joinToString(separator = "") { byte -> "%02x".format(byte) }
 
 /**
  * Return the SHA-1 for the provided stream.
  */
-@Suppress("ControlFlowWithEmptyBody")
 fun sha1(stream: InputStream): String {
     val md = MessageDigest.getInstance("SHA-1")
-    val dis = DigestInputStream(BufferedInputStream(stream), md)
-    stream.use {
-        while (dis.read() != -1) {
+    DigestInputStream(BufferedInputStream(stream), md).use { dis ->
+        val buffer = ByteArray(BUFFER_SIZE)
+        while (dis.read(buffer) != -1) {
+            // Read until EOF
         }
     }
     return md.digest().toHex()
